2b6e1aad357c8e3f307ad2a93eeec4b5309144fd
Christian Blume <chr.blume@gmail.com>
2017-11-12 22:08:14 +1300

    persist hashstore to disk

--- python/hashstore/store.py
+++ python/hashstore/store.py
@@ -1,18 +1,28 @@
+import pickle
+import os
 
 
 class Store:
 
     def __init__(self):
-        self.s = {}
+        thisdir = os.path.dirname(os.path.abspath(__file__))
+        self.filename = os.path.join(thisdir, 'hashstore.db')
+        if os.path.exists(self.filename):
+            with open(self.filename, 'rb') as f:
+                self.s = pickle.load(f)
+        else:
+            self.s = {}
 
     def clear(self):
         self.s.clear()
+        self._save()
 
     def size(self):
         return len(self.s)
 
     def put(self, key, value):
         self.s[key] = value
+        self._save()
 
     def get(self, key):
         return self.s.get(key)
@@ -24,3 +34,8 @@ class Store:
 
     def remove(self, key):
         del self.s[key]
+        self._save()
+
+    def _save(self):
+        with open(self.filename, 'wb') as f:
+            pickle.dump(self.s, f)
