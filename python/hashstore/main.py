a6b718e6cf19c4da35552196843539fbe0e8d981
Guan Wang <ggwangguan@gmail.com>
2018-01-04 21:58:50 +1300

    add initial UI

--- python/hashstore/main.py
+++ python/hashstore/main.py
@@ -2,4 +2,4 @@ from hashstore.routes import app
 
 
 if __name__ == '__main__':
-    app.run('localhost', 5001, debug=True)
+    app.run('127.0.0.1', 5001, debug=True)
--- ui/app/main.py
+++ ui/app/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask.templating import render_template
+import requests
 
 app = Flask(__name__)
+
+
+def req(modifier, params):
+    r = requests.get("http://127.0.0.1:5001/" + modifier, params=params)
+    return r.text, r.status_code
 
 
 @app.route("/")
@@ -9,5 +15,30 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/clear")
+def clear():
+    return req("clear", request.args)
+
+
+@app.route("/size")
+def size():
+    return req("size", request.args)
+
+
+@app.route("/get")
+def get():
+    return req("get", request.args)
+
+
+@app.route("/put")
+def put():
+    return req("put", request.args)
+
+
+@app.route("/remove")
+def remove():
+    return req("remove", request.args)
+
+
 if __name__ == '__main__':
-    app.run('localhost', 5000, debug=True)
+    app.run('127.0.0.1', 5000, debug=True)
